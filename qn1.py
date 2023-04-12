dc59fb9fe546cca3adffffa314493a66fcedb392
Nishant <bhusalnishant93@gmail.com>
2023-04-12 09:00:03 -0400

    made first changes in the first qn

---
+++ qn1.py
@@ -0,0 +1,26 @@
+#   TAKING INPUt FROM THE USER 
+x=int(input("enter the number of years: "))
+for i in range(x):
+    total_rainfall=0       #  INITIALISING THE RAINFALL AMOUNT
+    for j in range(12):     # NESTED LOOPING TO ASK FOR THE RAINFALL IN 12 MONTHS AND ITERATING THEM 
+        rain=int(input("enetr the rainfall amount fot the month",j+1,":" ))
+        rain+=1
+    total_rainfall.append(rain)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
